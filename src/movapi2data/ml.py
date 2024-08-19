88155fcfd602569b7555023e2ecd82dd4f4fbac7
lsiwh37249 <lsiwh37249@gmail.com>
2024-08-19 20:51:51 +0900

    전체적인 flow 확인

--- src/movapi2data/ml.py
+++ src/movapi2data/ml.py
@@ -1,10 +1,21 @@
 import requests
 import os
+import json
 
 API_KEY = os.getenv('MOVIE_API_KEY')
 
+
 def data2json(year=2015):
-    url = f"http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key={API_KEY}"
-    data = requests.get(url)
-    print(data)
-    return data
+    file_path = f'/home/kim1/data/movies/year={year}/data.json'
+    base_url = f"http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key={API_KEY}&openStartDt={year}&openEndDt={year}"
+    data = requests.get(base_url)
+    j = data.json()
+    #print(data)
+    print(j)
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    with open(file_path, 'w', encoding='utf8' ) as f:
+        json.dump(j, f, indent=4, ensure_ascii=False)
+
+
+    return data, file_path
+
