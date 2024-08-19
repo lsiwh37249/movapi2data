67bb22354a4956d18667a1df03da5507253a7225
lsiwh37249 <lsiwh37249@gmail.com>
2024-08-19 20:52:08 +0900

    전체적인 flow 확인

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -1,5 +1,8 @@
 from  movapi2data.ml import data2json
-
+import os
 def test_data2json():
-    data = data2json()  
+    data,file_path = data2json() 
+    #print(data)
     assert data
+    assert os.path.exists(file_path)
+    
